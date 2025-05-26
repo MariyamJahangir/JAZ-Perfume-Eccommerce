7959a889e28a0d55aa41729a5419bc34d125b944
Mariyam Jahangir <jasjahan123@gmail.com>
2025-05-26 18:23:54 +0400

    final stage

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => { 
     try {
-        const conn = await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
-        //const conn = await mongoose.connect(`${process.env.DB_URL}`);
+        //const conn = await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+        const conn = await mongoose.connect(`${process.env.DB_URL}`);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
