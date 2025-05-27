97591fe2be0cb2d436ac454be88037cd325df027
Mariyam Jahangir <jasjahan123@gmail.com>
2025-05-27 17:09:07 +0400

    final stage

--- config/productMulter.js
+++ config/productMulter.js
@@ -1,9 +1,36 @@
-const multer = require('multer');
+// const multer = require('multer');
 
+
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, "uploads/"); // Define your uploads folder
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, `${Date.now()}-${file.originalname}`);
+//     },
+// });
+
+// const upload = multer({ storage });
+
+// module.exports = {
+//     upload
+// }
+
+
+
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+const uploadPath = path.join(__dirname, 'uploads');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/"); // Define your uploads folder
+        // Check if 'uploads' folder exists, if not, create it
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        }
+        cb(null, uploadPath);
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`);
@@ -14,4 +41,4 @@ const upload = multer({ storage });
 
 module.exports = {
     upload
-}
+};
